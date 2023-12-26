7daa800181c068709e914bed32ef5ab2da4fee85
Cesar Augusto <gutoamorim@outlook.com>
2023-12-26 10:39:46 -0300

    Adicionado SVG aos cards do componente balance

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,11 +1,14 @@
 import Card from "./Card";
+import income from "../assets/income.svg";
+import expense from "../assets/expense.svg";
+import total from "../assets/total.svg";
 
 export default function Balance() {
   return (
     <section className="container m-auto flex items-center justify-around -mt-10">
-      <Card type="Entradas" bg="bg-lime-500" color="text-white" />
-      <Card type="Saídas" bg="bg-red-500" color="text-white" />
-      <Card type="Total" bg="bg-white" />
+      <Card type="Entradas" bg="bg-lime-500" color="text-white" svg={income} />
+      <Card type="Saídas" bg="bg-red-400" color="text-white" svg={expense} />
+      <Card type="Total" bg="bg-white" svg={total} />
     </section>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,10 @@
-export default function Card({ type, bg, color }) {
+export default function Card({ type, bg, color, svg }) {
   return (
     <div className={`p-4 rounded-2xl shadow-lg ${bg} w-60`}>
-      <h1 className={`${color}`}>{type}</h1>
+      <h1 className={`flex items-center justify-between ${color}`}>
+        {type}
+        <img src={svg} alt="" />
+      </h1>
       <p className={`text-2xl font-semibold ${color}`}>R$ 5.000,00</p>
     </div>
   );
