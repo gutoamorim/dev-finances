dadc47a0f2a11997af21a892f2f6585e264a400d
Cesar Augusto <gutoamorim@outlook.com>
2023-12-26 17:56:02 -0300

    Adicionado SVG a tabela de transações

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,5 +1,7 @@
 import { useContext } from "react";
 import { TransactionContext } from "../contexts/TransactionContext";
+import edit from "../assets/edit.svg";
+import trash from "../assets/trash.svg";
 
 export default function Table() {
   const transactions = useContext(TransactionContext);
@@ -12,7 +14,7 @@ export default function Table() {
             <th className="py-4 px-2">Descrição</th>
             <th>Valor</th>
             <th>Data</th>
-            <th>Ações</th>
+            <th className="max-w-min"></th>
           </tr>
         </thead>
         <tbody className="bg-slate-200">
@@ -27,7 +29,24 @@ export default function Table() {
                   })}
                 </td>
                 <td>{transaction.date}</td>
-                <td></td>
+                <td className="w-1/12">
+                  <button>
+                    <img
+                      className="w-5 h-5"
+                      src={edit}
+                      alt="Editar"
+                      title="Editar"
+                    />
+                  </button>
+                  <button>
+                    <img
+                      className="w-5 h-5 ml-5"
+                      src={trash}
+                      alt="Excluir"
+                      title="Excluir"
+                    />
+                  </button>
+                </td>
               </tr>
             ))}
         </tbody>
