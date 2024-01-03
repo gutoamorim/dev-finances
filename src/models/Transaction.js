7619c43a9acb3b0908a9c8bb6b8df6d5e41591cd
Cesar Augusto <gutoamorim@outlook.com>
2024-01-03 11:59:23 -0300

    Classe de modelos atualizada

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -1,9 +1,9 @@
 export class Transaction {
-  constructor({ type, description, amount, date }) {
+  constructor({ description, amount, date }) {
     this.id = Math.floor(Math.random() * 100000000);
-    (this.description = description),
-      (this.amount = +amount),
-      // (this.date = new Date(date));
-      (this.date = date);
+    this.description = description;
+    this.amount = +amount;
+    this.date = date;
+    // (this.date = new Date(date));
   }
 }
