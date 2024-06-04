1e8981088dbd81b19134f85021f927ed8022efb5
Cesar Augusto <gutoamorim@outlook.com>
2024-06-03 22:30:09 -0300

    adicionando transações no localstorage e renderizando na tela

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const transactions = [];
+let transactions = [];
 let id = 0;
 
 const add = document.querySelector("#new");
@@ -8,6 +8,15 @@ let body = document.createElement("div");
 body.classList.add("modal");
 
 add.addEventListener("click", () => toggleModal("new"));
+
+function setLocalStorage(transactions) {
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+}
+
+function getLocalStorage() {
+  const transactions = localStorage.getItem("transactions");
+  return JSON.parse(transactions);
+}
 
 function toggleModal(type) {
   modal.classList.toggle("active");
@@ -21,38 +30,76 @@ function renderModal(type) {
   body.innerHTML = "";
   if (type) {
     if (type === "new") {
-      body.innerHTML = `
-        <h1>Adicionar transação</h1>
-        <div class="modal-content">
-          <div class="type-transaction-container">
-            <div class="type-transaction" id="income" onclick="renderModal(this.id)">
-              <img src="./assets/income.svg" alt="Imagem ilustrativa do card entradas">  
-              Receita
-            </div>
-              <div class="type-transaction" id="expense"  onclick="renderModal(this.id)">
-              <img src="./assets/expense.svg" alt="Imagem ilustrativa do card entradas">  
-              Despeza
-            </div>
-          </div>
-        </div>
-      `;
+      body.innerHTML = renderNew();
     } else if (type === "income" || type === "expense") {
-      body.innerHTML = `
-      <h1>Adicionar ${type === "income" ? "receita" : "despeza"}</h1>
-      <div class="modal-content">
-        <form>
-          <input type="text" id="description" placeholder="Descrição..."/>
-          <input type="text" id="amount" placeholder="R$ 0,00"/>
-          <input type="date" id="date"/>
-          <div class="modal-actions">
-            <span class="button cancel" onclick="toggleModal()">cancelar</span>
-            <span class="button save" id="${type}" onclick="handleTransaction(this.id)">Salvar</span>
-          </div>
-        </form>
-      </div>
-    `;
+      body.innerHTML = renderForm(type);
     }
     modal.appendChild(body);
+  }
+}
+
+function renderNew() {
+  return `
+  <h1>Adicionar transação</h1>
+  <div class="modal-content">
+    <div class="type-transaction-container">
+      <div class="type-transaction" id="income" onclick="renderModal(this.id)">
+        <img src="./assets/income.svg" alt="Imagem ilustrativa do card entradas">  
+        Receita
+      </div>
+        <div class="type-transaction" id="expense"  onclick="renderModal(this.id)">
+        <img src="./assets/expense.svg" alt="Imagem ilustrativa do card entradas">  
+        Despeza
+      </div>
+    </div>
+  </div>
+`;
+}
+
+function renderForm(type) {
+  return `
+  <h1>Adicionar ${type === "income" ? "receita" : "despeza"}</h1>
+  <div class="modal-content">
+    <form>
+      <input type="text" id="description" placeholder="Descrição..."/>
+      <input type="text" id="amount" placeholder="R$ 0,00"/>
+      <input type="date" id="date"/>
+      <div class="modal-actions">
+        <span class="button cancel" onclick="toggleModal()">cancelar</span>
+        <span class="button save" id="${type}" onclick="handleTransaction(this.id)">Salvar</span>
+      </div>
+    </form>
+  </div>
+`;
+}
+
+function renderTransactions() {
+  transactions = getLocalStorage() || [];
+
+  const tbody = document.querySelector("tbody");
+  tbody.innerHTML = "";
+
+  if (transactions && transactions.length > 0) {
+    transactions.forEach((transaction) => {
+      const tr = document.createElement("tr");
+      tr.innerHTML = `
+      <td>${transaction.description}</td>
+      <td>${transaction.amount}</td>
+      <td>${transaction.date}</td>
+      <td>
+          <div class="actions">
+              <img src="./assets/edit.svg" alt="">
+              <img src="./assets/trash.svg" alt="">
+          </div>
+      </td>
+      `;
+
+      tbody.appendChild(tr);
+    });
+  } else {
+    const tr = document.createElement("tr");
+    tr.innerHTML = `<td style="background-color: #f0f2f5; height: 3rem;" colspan=4><p style="text-align: center;">Você não possui transações adicionadas.</p></td>`;
+    tbody.appendChild(tr);
   }
 }
 
@@ -78,5 +125,9 @@ function handleTransaction(type) {
 
 function saveTransaction(transaction) {
   transactions.push(transaction);
+  setLocalStorage(transactions);
   toggleModal();
+  renderTransactions();
 }
+
+renderTransactions();
